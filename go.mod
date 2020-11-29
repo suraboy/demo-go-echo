39d45900155eec5d82644f7ecb161651dabfb714
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2020-11-29 10:43:59 +0700

    initial commit

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module demo
+module go-echo
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+)
--- server.go
+++ server.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
+	//"github.com/suraboy/go-echo/api/routes"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World2!")
-	})
+	//routes.init()
+	//e.GET("/", func(c echo.Context) error {
+	//	return c.String(http.StatusOK, "Hello, World WTF!")
+	//})
 	e.Logger.Fatal(e.Start(":8080"))
 }
