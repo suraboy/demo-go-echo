93b12978a97e01ceb59b8d1f5ebc7440eeff8411
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2020-11-29 19:03:08 +0700

    no message

--- server.go
+++ server.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	//"github.com/suraboy/go-echo/api/routes"
+	"net/http"
 )
 
 func main() {
 	e := echo.New()
-	//routes.init()
-	//e.GET("/", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "Hello, World WTF!")
-	//})
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World Ya Boy!")
+	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
