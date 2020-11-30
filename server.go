fdd03bf8e9a2bb9416ed440236962ac653fba6cd
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2020-12-01 04:59:05 +0700

    api create & find by id

--- server.go
+++ server.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"golang.org/x/crypto/bcrypt"
+	_ "golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -25,6 +27,8 @@ func main() {
 	h.connectDB()
 
 	e.GET("/v1/users", h.GetAllUser)
+	e.GET("/v1/users/:id", h.FindUser)
+	e.POST("/v1/users", h.CreateUser)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
@@ -46,7 +50,7 @@ func (h *Handler) connectDB() {
 	username := os.Getenv("DB_USERNAME")
 	password := os.Getenv("DB_PASSWORD")
 
-	dsn := username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+	dsn := username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&loc=Asia%2FBangkok&parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
@@ -56,58 +60,87 @@ func (h *Handler) connectDB() {
 
 // User
 type Users struct {
-	Id    uint    `json:"id"`
-	Username    string   `json:"username"`
-	Name  string `json:"name" xml:"name"`
-	LastName string `json:"last_name"`
-	Email string `json:"email" xml:"email"`
-	Verify string `json:"verify"`
-	Mobile string `json:"mobile"`
-	Type	string `json:"type"`
-	Pin string `json:"pin"`
-	Status string `json:"status"`
-	UserGroupId int `json:"user_group_id"`
-	Gender string `json:"gender"`
-	Birthday string `json:"birthday"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID          uint      `gorm:"primary_key" json:"id"`
+	Username    string    `json:"username"`
+	Password    string    `json:"password"`
+	Name        string    `json:"name" xml:"name"`
+	LastName    string    `json:"last_name"`
+	Email       string    `json:"email" xml:"email"`
+	Verify      string    `json:"verify" gorm:"type:enum('waiting','yes','no');default:'waiting'"`
+	Mobile      string    `json:"mobile"`
+	Type        string    `json:"type" gorm:"type:enum('owner','staff','other','admin','customer','brand-owner');default:'other'"`
+	Pin         string    `json:"pin"`
+	Status      string    `json:"status" gorm:"type:enum('active', 'inactive', 'ban');default:'inactive'"`
+	UserGroupId int       `json:"user_group_id"`
+	Gender      string    `json:"gender" gorm:"type:enum('male', 'female');default:'male'"`
+	Birthday    time.Time `json:"birthday"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type messageError struct {
+var messageError struct {
 	Errors messageFormat `json:"errors"`
 }
 
 type messageFormat struct {
 	StatusCode int    `json:"status_code"`
 	Message    string `json:"message"`
+	Error      string `json:"error"`
 }
 
-var TransformUser struct {
-	Data []Users `json:"data"`
+//get user list
+func (h *Handler) GetAllUser(c echo.Context) (err error) {
+	var user []Users
+	h.DB.Find(&user)
+	return c.JSON(http.StatusOK, echo.Map{"data": user})
 }
 
-func (h *Handler) GetAllUser(c echo.Context) error {
-	//id := c.Param("id")
-	rows, err := h.DB.Model(&Users{}).Rows()
-	defer rows.Close()
+func (h *Handler) FindUser(c echo.Context) (err error) {
+	id := c.Param("id")
+	user := Users{}
 
-	if err != nil {
-		errorJson := `{"errors":{"status_code":404,"message":"Not Found."}}`
-		var errors messageError
-		json.Unmarshal([]byte(errorJson), &errors)
-
-		return c.JSON(http.StatusNotFound, errors)
-	}
-
-	for rows.Next() {
-		var userData Users
-		// ScanRows scan a row into user
-		err := h.DB.ScanRows(rows, &userData)
-		if err != nil {
-			panic(err.Error())
+	if err := h.DB.Find(&user, id).Error; err != nil || h.DB.Find(&user, id).RowsAffected == 0 {
+		var msgError messageFormat
+		if h.DB.Find(&user, id).RowsAffected == 0 {
+			msgError.StatusCode = http.StatusNotFound
+			msgError.Message = "Not Found"
+		}else{
+			msgError.StatusCode = http.StatusInternalServerError
+			msgError.Message = "Internal Server Error"
+			msgError.Error = err.Error()
 		}
-		TransformUser.Data = append(TransformUser.Data,userData)
+		messageError.Errors = msgError
+		return c.JSON(msgError.StatusCode, messageError)
 	}
 
-	return c.JSON(200, TransformUser)
+	return c.JSON(http.StatusOK, echo.Map{"data": user})
+}
+
+//create user
+func (h *Handler) CreateUser(c echo.Context) (err error) {
+	user := new(Users)
+	if err = c.Bind(user); err != nil {
+		var msgError messageFormat
+		msgError.StatusCode = http.StatusBadRequest
+		msgError.Message = "Bad Request"
+		msgError.Error = err.Error()
+		messageError.Errors = msgError
+		return c.JSON(http.StatusBadRequest, messageError)
+	}
+	password := []byte(user.Password)
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	user.Password = string(hashedPassword)
+	if err := h.DB.Create(&user).Error; err != nil {
+		var msgError messageFormat
+		msgError.StatusCode = http.StatusExpectationFailed
+		msgError.Message = "Expectation Failed"
+		msgError.Error = err.Error()
+		messageError.Errors = msgError
+		return c.JSON(http.StatusExpectationFailed, messageError)
+	} // pass pointer of data to Create
+
+	return c.JSON(http.StatusCreated, echo.Map{"data": user})
 }
