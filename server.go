1c895d474ad2683476ce2e98f2d4677e0f617e5a
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2020-12-14 02:47:39 +0700

    fixed docker file & supervisor

--- server.go
+++ server.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	//"github.com/suraboy/go-echo/routes"
+	"github.com/suraboy/go-echo/routes"
 	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"log"
 	"net/http"
 	"os"
-	"log"
 	"time"
 )
 
@@ -28,7 +28,7 @@ func main() {
 	//e.GET("/v1/users", h.GetAllUser)
 	//e.GET("/v1/users/:id", h.FindUser)
 	//e.POST("/v1/users", h.CreateUser)
-	//routes.UserRoute(e)
+	routes.UserRoute(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -45,6 +45,7 @@ func (h *Handler) connectDB() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	dbConnection := os.Getenv("DB_CONNECTION")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_DATABASE")
@@ -52,7 +53,8 @@ func (h *Handler) connectDB() {
 	password := os.Getenv("DB_PASSWORD")
 
 	dsn := username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&loc=Asia%2FBangkok&parseTime=true"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(dbConnection, dsn)
+
 	if err != nil {
 		log.Fatal(err.Error())
 	}
